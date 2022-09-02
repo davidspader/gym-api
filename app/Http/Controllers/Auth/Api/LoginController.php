4bde8b5f947713bb31305c598c0ed7ae208a866a
David <davidspader97@gmail.com>
2022-09-02 20:59:02 -0300

    create login, logout and register to user with sanctum

---
+++ app/Http/Controllers/Auth/Api/LoginController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Auth\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if(!auth()->attempt($credentials)) abort(401, 'Invalid credentials');
+
+        $token = auth()->user()->createToken('auth_token');
+
+        return response()->json([
+            'data' => [
+                'token' => $token->plainTextToken
+            ]
+        ]);
+    }
+
+    public function logout()
+    {
+        auth()->user()->currentAccessToken()->delete();
+
+        return response()->json([], 204);
+    }
+}
