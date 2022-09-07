6b66e6d273dbeb6e391ad33c35278ea52b2b3acb
David <davidspader97@gmail.com>
2022-09-07 15:57:28 -0300

    move users tests to API/user/

--- tests/Feature/API/user/LoginControllerTest.php
+++ tests/Feature/API/user/LoginControllerTest.php
@@ -1,10 +1,9 @@
 <?php
 
-namespace Tests\Feature\API;
+namespace Tests\Feature\API\user;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
