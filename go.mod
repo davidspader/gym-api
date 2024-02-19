ef6b48ce79293f890dbce93dc0e9c3ead865c1c8
David Ambrozio <davidambroziodev@gmail.com>
2024-02-19 14:40:14 -0300

    create users model

--- go.mod
+++ go.mod
@@ -11,3 +11,5 @@ require github.com/lib/pq v1.10.9
 require golang.org/x/crypto v0.19.0
 
 require github.com/dgrijalva/jwt-go v3.2.0+incompatible
+
+require github.com/badoux/checkmail v1.2.4
---
+++ src/models/User.go
@@ -0,0 +1,65 @@
+package models
+
+import (
+	"errors"
+	"gym-api/src/security"
+	"strings"
+
+	"github.com/badoux/checkmail"
+)
+
+type User struct {
+	ID       uint64 `json:"id,omitempty"`
+	Name     string `json:"name,omitempty"`
+	Email    string `json:"email,omitempty"`
+	Password string `json:"password,omitempty"`
+}
+
+func (user *User) Prepare(step string) error {
+	if err := user.validate(step); err != nil {
+		return err
+	}
+
+	if err := user.format(step); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (user *User) validate(step string) error {
+	if user.Name == "" {
+		return errors.New("name is required and cannot be blank")
+	}
+
+	if user.Email == "" {
+		return errors.New("email is required and cannot be blank")
+	}
+
+	if erro := checkmail.ValidateFormat(user.Email); erro != nil {
+		return errors.New("the entered email is invalid")
+	}
+
+	if step == "register" && user.Password == "" {
+		return errors.New("password is required and cannot be blank")
+	}
+
+	return nil
+}
+
+func (user *User) format(step string) error {
+	user.Name = strings.TrimSpace(user.Name)
+	user.Email = strings.TrimSpace(user.Email)
+	user.Password = strings.TrimSpace(user.Password)
+
+	if step == "register" {
+		hashPassword, err := security.GenerateHash(user.Password)
+		if err != nil {
+			return err
+		}
+
+		user.Password = string(hashPassword)
+	}
+
+	return nil
+}
