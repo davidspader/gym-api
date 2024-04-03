706236ab23bc14b8de177ad8e38e10cc990b522e
David Ambrozio <davidambroziodev@gmail.com>
2024-04-03 14:50:20 -0300

    refactor methods order

--- src/controllers/workouts.go
+++ src/controllers/workouts.go
@@ -59,6 +59,67 @@ func CreateWorkout(w http.ResponseWriter, r *http.Request) {
 	responses.SendJSON(w, http.StatusCreated, workout)
 }
 
+func GetWorkoutsByUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	userID, err := strconv.ParseUint(params["userId"], 10, 64)
+	if err != nil {
+		responses.SendError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	userIDInToken, err := auth.ExtractUserID(r)
+	if err != nil {
+		responses.SendError(w, http.StatusUnauthorized, err)
+		return
+	}
+
+	if userID != userIDInToken {
+		err = errors.New("it is not possible to view another user's workouts")
+		responses.SendError(w, http.StatusForbidden, err)
+		return
+	}
+
+	db, err := database.Connect()
+	if err != nil {
+		responses.SendError(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer db.Close()
+
+	repo := repository.NewWorkoutsRepository(db)
+	workouts, err := repo.GetWorkoutsByUserID(userID)
+	if err != nil {
+		responses.SendError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	responses.SendJSON(w, http.StatusOK, workouts)
+}
+
+func UpdateWorkout(w http.ResponseWriter, r *http.Request) {
+	userID, err := auth.ExtractUserID(r)
+	if err != nil {
+		responses.SendError(w, http.StatusUnauthorized, err)
+		return
+	}
+
+	params := mux.Vars(r)
+	workoutID, err := strconv.ParseUint(params["workoutId"], 10, 64)
+	if err != nil {
+		responses.SendError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	db, err := database.Connect()
+	if err != nil {
+		responses.SendError(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer db.Close()
+
+	fmt.Printf(string(rune(userID)), workoutID)
+}
+
 func AddExercises(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	userID, err := strconv.ParseUint(params["userId"], 10, 64)
@@ -122,65 +183,4 @@ func AddExercises(w http.ResponseWriter, r *http.Request) {
 	}
 
 	responses.SendJSON(w, http.StatusCreated, nil)
-}
-
-func GetWorkoutsByUser(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	userID, err := strconv.ParseUint(params["userId"], 10, 64)
-	if err != nil {
-		responses.SendError(w, http.StatusBadRequest, err)
-		return
-	}
-
-	userIDInToken, err := auth.ExtractUserID(r)
-	if err != nil {
-		responses.SendError(w, http.StatusUnauthorized, err)
-		return
-	}
-
-	if userID != userIDInToken {
-		err = errors.New("it is not possible to view another user's workouts")
-		responses.SendError(w, http.StatusForbidden, err)
-		return
-	}
-
-	db, err := database.Connect()
-	if err != nil {
-		responses.SendError(w, http.StatusInternalServerError, err)
-		return
-	}
-	defer db.Close()
-
-	repo := repository.NewWorkoutsRepository(db)
-	workouts, err := repo.GetWorkoutsByUserID(userID)
-	if err != nil {
-		responses.SendError(w, http.StatusInternalServerError, err)
-		return
-	}
-
-	responses.SendJSON(w, http.StatusOK, workouts)
-}
-
-func UpdateWorkout(w http.ResponseWriter, r *http.Request) {
-	userID, err := auth.ExtractUserID(r)
-	if err != nil {
-		responses.SendError(w, http.StatusUnauthorized, err)
-		return
-	}
-
-	params := mux.Vars(r)
-	workoutID, err := strconv.ParseUint(params["workoutId"], 10, 64)
-	if err != nil {
-		responses.SendError(w, http.StatusBadRequest, err)
-		return
-	}
-
-	db, err := database.Connect()
-	if err != nil {
-		responses.SendError(w, http.StatusInternalServerError, err)
-		return
-	}
-	defer db.Close()
-
-	fmt.Printf(string(rune(userID)), workoutID)
 }
